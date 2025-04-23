2c6ceb0d6bd153e41e23fbd2ad8971a332b6bd66
KonradBi <konrad@Laptop-von-Konrad.local>
2025-04-23 19:48:46 +0800

    chore: remove duplicate legal links from bottom footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-gray-400 py-6 mt-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-sm">
         <p>&copy; {currentYear} KDP Ninja. All rights reserved.</p>
-        <div className="mt-2">
-          {/* Use standard <a> tags for testing */}
-          <a href="/terms" className="hover:text-indigo-400 transition duration-150 ease-in-out">
-            Terms of Service
-          </a>
-          <span className="mx-2">|</span>
-          <a href="/privacy" className="hover:text-indigo-400 transition duration-150 ease-in-out">
-            Privacy Policy
-          </a>
-        </div>
+        
       </div>
     </footer>
   );
